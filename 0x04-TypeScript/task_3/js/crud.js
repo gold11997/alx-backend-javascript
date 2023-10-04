cb11c891eb51f9ef771805dfab055cd87ce9508c
gold11997 <abrahambenson90@gmail.com>
2023-10-04 23:12:09 +0100

    Printing teachers

---
+++ 0x04-TypeScript/task_3/js/crud.js
@@ -0,0 +1,5 @@
+export function insertRow(row) {
+    console.log('Insert row', row);
+    return Math.floor(Math.random() * Math.floor(1000));
+}
+
---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,5 @@
+import { RowID, RowElement } from "./interface";
+import * as CRUD from "./crud";
+import { insertRow } from "./crud";
+
+const row : RowElement = {
